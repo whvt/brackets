c55d6ea749fe8e5bd1a4860bc3f2c47089341df2
whvt <60581769+whvt@users.noreply.github.com>
2020-03-12 14:51:15 +0300

    done brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let br = bracketsConfig.map( (b) => b.join("") );
+  for (let i = 0; i < br.length; i++) {
+    if( str.includes(br[i]) ) {
+      str = str.replace(br[i], "");
+      i = -1;
+    }
+  }
+  return (str) ? false : true;
+};
